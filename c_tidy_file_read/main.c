373517bdf8e839a027391fe11f75ab410cce36b7
rustymagnet3000 <rustymagnet3000@gmail.com>
2018-01-01 16:34:57 -0600

    Added remote repo and first commit all from xCode 9.

--- c_tidy_file_read/main.c
+++ c_tidy_file_read/main.c
@@ -1,15 +1,7 @@
-//
-//  main.c
-//  c_tidy_file_read
-//
-//  Created by rustymagnet on 01/01/2018.
-//  Copyright © 2018 rusty_magnet_org. All rights reserved.
-//
-
+//  Created by rustymagnet on 01/01/2018
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Hello, World!\n");
     return 0;
 }
