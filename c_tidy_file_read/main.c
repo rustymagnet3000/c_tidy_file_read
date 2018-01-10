0eacc0a766daa9dd0524918a059fa079246363df
rustymagnet3000 <rustymagnet3000@gmail.com>
2018-01-10 19:29:08 +0000

    updated the time difference functions.

--- c_tidy_file_read/main.c
+++ c_tidy_file_read/main.c
@@ -30,50 +30,62 @@ enum TIME_TAKEN yd_time_taken(unsigned long *result) {
     return WEEKS;
 }
 
-char* yd_set_readable_string(unsigned long *result) {
-
-    int bufSize = 5;
-    char *buf = malloc(bufSize); // +1 not required due to >= check
+/* pass by reference */
+void yd_combine_string_data(int *size_of_buffer, char *buffer, int result, int remainder, char *time_value) {
     
-    if(snprintf(buf, bufSize, "%lu Seconds woohoop", *result) >= bufSize){
-        bufSize *= 2;
-        free(buf);
-        buf = malloc(bufSize);
-        
-        if(snprintf(buf, bufSize, "%lu Seconds", *result) >= bufSize){
+    // inline check an snprintf operation
+    if(snprintf(buffer, *size_of_buffer, "%lu %s", result, time_value) >= *size_of_buffer){
+        *size_of_buffer *= 2;
+        free(buffer);
+        buffer = malloc(*size_of_buffer);
+
+        if(snprintf(buffer, *size_of_buffer, "%lu Seconds woohoop", result) >= *size_of_buffer){
             printf("Still not enough space. Aborting\n");
             exit(1);
         }
     }
+}
+
+char* yd_set_readable_string(unsigned long *result) {
+
+    int bufSize = 30;
+    char *buf = malloc(bufSize); // +1 not required due to >= check
+    char *time_value;
+    div_t result_summary;
     
     switch (yd_time_taken(result)) {
         case SECONDS:
-            return buf;
+            time_value = "seconds";break;
         case MINUTES:
-            return "MINUTES in a nice format";
+            result_summary = div(*result, 60);
+            time_value = "minutes";break;
         case HOURS:
-            return "HOURS in a nice format";
+            time_value = "hours";break;
         case DAYS:
-            return "DAYS in a nice format";
+            time_value = "days";break;
         default:
-            return "unhandled - default to weeks";
+            *result = 666;
+            time_value = "weeks";break;
     }
+    
+    yd_combine_string_data(&bufSize, buf, result_summary.quot, result_summary.rem, time_value);
+    return buf;
 }
 
 int main() {
     
     // console_header();
     YD_TIME time_start = yd_init_time();
-    sleep(2);
+    sleep(1);
     YD_TIME time_end = yd_init_time();;
     
     unsigned long time_taken = yd_calculate_time_taken(&time_end.epoch_time, &time_start.epoch_time);
-
+    
     char *time_taken_lbl = yd_set_readable_string(&time_taken);
     
     console_io(time_taken_lbl, time_taken_lbl);
     
-    //    read_file_line_by_line();
+    // read_file_line_by_line();
     console_footer();
     
     return 0;
