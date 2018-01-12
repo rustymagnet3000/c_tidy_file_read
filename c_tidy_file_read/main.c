eb9100ef867f37e68d530bb3d4f56504dc4f70e4
rustymagnet3000 <rustymagnet3000@gmail.com>
2018-01-12 18:18:35 +0000

    method one of showing time label has a logic problem.

--- c_tidy_file_read/main.c
+++ c_tidy_file_read/main.c
@@ -5,6 +5,8 @@
 #include "console_io.h"
 #include "yd_time.h"
 
+/* this is a large convenience initializor. You set the total seconds and then all other pieces are auto calculated */
+
 enum TIME_TAKEN{
     SECONDS,
     MINUTES,
@@ -12,6 +14,15 @@ enum TIME_TAKEN{
     DAYS,
     WEEKS
 };
+
+typedef struct YD_TIME_RESULT {
+    char *primary_time_value;
+    char *secondary_time_value;
+    unsigned long total_seconds;
+    int remainder;
+    int quot;
+    int divisor;
+} YD_TIME_RESULT;
 
 enum TIME_TAKEN yd_time_taken(unsigned long *result) {
   
@@ -31,61 +42,90 @@ enum TIME_TAKEN yd_time_taken(unsigned long *result) {
 }
 
 /* pass by reference */
-void yd_combine_string_data(int *size_of_buffer, char *buffer, int result, int remainder, char *time_value) {
+void yd_combine_string_data(int *size_of_buffer, char *buffer, YD_TIME_RESULT *time_result) {
+    
+    time_result->quot = (int) time_result->total_seconds/time_result->divisor;
+    
+    if (time_result->total_seconds % 60 == 0)
+    {
+        time_result->remainder = 0;
+    } else {
+        time_result->remainder = time_result->total_seconds%60;
+    }
     
     // inline check an snprintf operation
-    if(snprintf(buffer, *size_of_buffer, "%lu %s", result, time_value) >= *size_of_buffer){
+    if(snprintf(buffer,
+                    *size_of_buffer, "%d %s %d %s",
+                    time_result->quot,
+                    time_result->primary_time_value,
+                    time_result->remainder,
+                    time_result->secondary_time_value) >= *size_of_buffer){
         *size_of_buffer *= 2;
         free(buffer);
         buffer = malloc(*size_of_buffer);
 
-        if(snprintf(buffer, *size_of_buffer, "%lu Seconds woohoop", result) >= *size_of_buffer){
+        if(snprintf(buffer,
+                    *size_of_buffer, "%d %s %d %s",
+                    time_result->quot,
+                    time_result->primary_time_value,
+                    time_result->remainder,
+                    time_result->secondary_time_value) >= *size_of_buffer){
             printf("Still not enough space. Aborting\n");
             exit(1);
         }
     }
 }
 
-char* yd_set_readable_string(unsigned long *result) {
+char* yd_set_readable_string(YD_TIME_RESULT *time_result) {
 
     int bufSize = 30;
     char *buf = malloc(bufSize); // +1 not required due to >= check
-    char *time_value;
-    div_t result_summary;
-    
-    switch (yd_time_taken(result)) {
+
+    switch (yd_time_taken(&time_result->total_seconds)) {
         case SECONDS:
-            time_value = "seconds";break;
+            time_result->primary_time_value ="seconds";
+            time_result->secondary_time_value="";
+            time_result->divisor=1;
+            break;
         case MINUTES:
-            result_summary = div(*result, 60);
-            time_value = "minutes";break;
+            time_result->primary_time_value ="minutes";
+            time_result->secondary_time_value="seconds";
+            time_result->divisor=60;
+            break;
         case HOURS:
-            time_value = "hours";break;
+            time_result->primary_time_value ="hours";
+            time_result->secondary_time_value="minutes";
+            time_result->divisor=3600;
+            break;
         case DAYS:
-            time_value = "days";break;
+            //            fallthrough;
         default:
-            *result = 666;
-            time_value = "weeks";break;
+            //            tba
+            break;
     }
     
-    yd_combine_string_data(&bufSize, buf, result_summary.quot, result_summary.rem, time_value);
+    yd_combine_string_data(&bufSize, buf, time_result);
     return buf;
 }
 
+
+// logic is incorrect.  I need a recursive loop that generates an array of values to show.  Starts at weeks and iterates down through each value.  Instead of trying to convert all into minutes or hours.  It has to be and.
+
 int main() {
     
-    // console_header();
+    console_header();
     YD_TIME time_start = yd_init_time();
-    sleep(1);
     YD_TIME time_end = yd_init_time();;
-    
-    unsigned long time_taken = yd_calculate_time_taken(&time_end.epoch_time, &time_start.epoch_time);
-    
-    char *time_taken_lbl = yd_set_readable_string(&time_taken);
-    
+    YD_TIME_RESULT time_result;
+
+    /* initialize time result */
+    time_result.total_seconds = yd_calculate_time_taken(&time_end.epoch_time, &time_start.epoch_time);
+
+    /* print result */
+    char *time_taken_lbl = yd_set_readable_string(&time_result);
     console_io(time_taken_lbl, time_taken_lbl);
-    
-    // read_file_line_by_line();
+
+//     read_file_line_by_line();
     console_footer();
     
     return 0;
