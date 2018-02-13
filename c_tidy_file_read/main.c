158b1a2c3d20a196aef08491d2022bf74f6bd43e
rustymagnet3000 <rustymagnet3000@gmail.com>
2018-02-13 05:03:38 +0000

    Merged all into main

--- c_tidy_file_read/main.c
+++ c_tidy_file_read/main.c
@@ -1,16 +1,11 @@
-//  Created by rustymagnet on 01/01/2018
 #include <stdio.h>  // sprintf
 #include <stdlib.h> // getline, malloc
 #include <unistd.h> // used for Sleep API
-#include "yd_file_read.h"  /* orchestrates file reading */
-#include "yd_console_io.h"
-#include "yd_time.h"
-#include "yd_calculate_time_taken.h"
-#include "search_file.h"
 #include "yd_handle_argv.h"
 
-/* next step: update to support file parsing                                    */
-/* then start to parse the file, on a background thread, while you load a menu  */
+/* Parse the file, on a background thread, while you load a menu    */
+/* Read the file line by line, into seperate Structs, when found    */
+/* then each menu option just reads through the Structs             */
 
 int main(int argc, const char * argv[]) {
 
